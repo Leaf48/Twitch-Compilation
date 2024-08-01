cdd3e3e8a99c13a343135465f890d5dde09242c2
leaf48 <watavita2004@gmail.com>
2024-08-01 13:30:45 +0900

    Update html.py

--- src/lib/streamlit_/html.py
+++ src/lib/streamlit_/html.py
@@ -1,9 +1,11 @@
 import streamlit as st
+from sys import platform
 
 
 def render_twitch_embed(slug: str):
+    parent = "localhost" if platform == "darwin" else "192.168.1.137"
     twitch_embed_code = f"""
-<iframe src="https://clips.twitch.tv/embed?clip={slug}&parent=localhost"
+<iframe src="https://clips.twitch.tv/embed?clip={slug}&parent={parent}"
     frameborder="0"
     allowfullscreen="true"
     scrolling="no"
